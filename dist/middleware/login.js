b4d134874c5fffe9d50518ef67121c8d707e4dcb
Cloviski <claudioribeiro.j@hotmail.com>
2024-01-30 17:30:00 -0300

    dist folder update

--- dist/middleware/login.js
+++ dist/middleware/login.js
@@ -3,17 +3,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.login = void 0;
 const jsonwebtoken_1 = require("jsonwebtoken");
 const login = (req, res, next) => {
-  try {
-    //It'll try to do a thing if it can't do something
-    const decode = (0, jsonwebtoken_1.verify)(
-      req.headers.authorization,
-      process.env.SECRET
-    ); // Will appear our token uncrypted
-    req.user = decode;
-    next(); //To continue, in this code is verify if the token is valid
-  } catch (error) {
-    return res.status(401).json({ message: "Not authorized" });
-  }
+    try {
+        //It'll try to do a thing if it can't do something
+        const decode = (0, jsonwebtoken_1.verify)(req.headers.authorization, process.env.SECRET); // Will appear our token uncrypted
+        req.user = decode;
+        next(); //To continue, in this code is verify if the token is valid
+    }
+    catch (error) {
+        return res.status(401).json({ message: "Not authorized" });
+    }
 };
 exports.login = login;
 //This file it'll verify if the user token is correct
--- dist/server.js
+++ dist/server.js
@@ -1,9 +1,7 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const user_routes_1 = require("./routes/user.routes");
@@ -13,19 +11,13 @@ const app = (0, express_1.default)();
 (0, dotenv_1.config)();
 const cors = require("cors");
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Context-Type, Accept"
-  );
-  res.header(
-    "Access-Control-Allow-Methods",
-    "POST, GET, PATCH, DELETE, OPTIONS"
-  );
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Context-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, OPTIONS");
+    next();
 });
 app.use(cors());
 app.use(express_1.default.json());
 app.use("/user", user_routes_1.userRoutes);
 app.use("/videos", videos_routes_1.videosRoutes);
-app.listen(4000, "0.0.0.0");
+app.listen(4000, '0.0.0.0');
